5b65f399b317e69dcbc70251d7cb0a876b36a9f3
Fırat Alçın <firatalcin04@gmail.com>
2024-03-23 23:19:46 +0300

    Contact ve Discount Controller'ları Tamamlandı

---
+++ RestaurantProject/RestaurantProject.SignalRApi/Controllers/ContactsController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantProject.Business.Interfaces;
+using RestaurantProject.Core.DataAccess.Dtos.CategoryDto;
+using RestaurantProject.Core.DataAccess.Dtos.ContactDto;
+using RestaurantProject.Entities.Concrete;
+
+namespace RestaurantProject.SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+        private readonly IMapper _mapper;
+
+        public ContactsController(IContactService contactService, IMapper mapper)
+        {
+            _contactService = contactService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var value = _mapper.Map<List<ResultContactDto>>(_contactService.GetAll());
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(CreateContactDto createContactDto)
+        {
+            _contactService.Add(_mapper.Map<Contact>(createContactDto));
+            return Ok("İletişim Bilgileri Eklenmiştir.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteContact(int id)
+        {
+            var value = _contactService.GetById(id);
+            _contactService.Delete(value);
+            return Ok("İletişim Bilgileri Silinmiştir.");
+        }
+
+        [HttpGet("GetContact")]
+        public IActionResult GetContact(int id)
+        {
+            var value = _contactService.GetById(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateContact(UpdateContactDto updateContactDto)
+        {
+            _contactService.Update(_mapper.Map<Contact>(updateContactDto));
+            return Ok("İletişim Bilgileri Güncellenmiştir.");
+        }
+    }
+}
---
+++ RestaurantProject/RestaurantProject.SignalRApi/Controllers/DiscountsController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantProject.Business.Interfaces;
+using RestaurantProject.Core.DataAccess.Dtos.DiscountDto;
+using RestaurantProject.Entities.Concrete;
+
+namespace RestaurantProject.SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+        private readonly IMapper _mapper;
+
+        public DiscountsController(IDiscountService discountService, IMapper mapper)
+        {
+            _discountService = discountService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult discountList()
+        {
+            var value = _mapper.Map<List<ResultDiscountDto>>(_discountService.GetAll());
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateDiscount(CreateDiscountDto createDiscountDto)
+        {
+            _discountService.Add(_mapper.Map<Discount>(createDiscountDto));
+            return Ok("İndirim Bilgileri Eklenmiştir.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteDiscount(int id)
+        {
+            var value = _discountService.GetById(id);
+            _discountService.Delete(value);
+            return Ok("İndirim Bilgileri Silinmiştir.");
+        }
+
+        [HttpGet("GetDiscount")]
+        public IActionResult GetDiscount(int id)
+        {
+            var value = _discountService.GetById(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateDiscount(UpdateDiscountDto updateDiscountDto)
+        {
+            _discountService.Update(_mapper.Map<Discount>(updateDiscountDto));
+            return Ok("İndirim Bilgileri Güncellenmiştir.");
+        }
+    }
+}
